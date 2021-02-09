424df093aa8d2195469d403bdd92108850b7ac29
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-09 22:25:16 +0530

    Defined folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Cards from './components/Cards/Cards';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Chart from './components/Chart/Chart';
+import styles from './App.module.css';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Covid Tracker</h1>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
 
 export default App;
