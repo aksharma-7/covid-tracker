6609027be5c6cdfc22320e4278287eea8862d7e5
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-10 19:34:23 +0530

    fixed typo

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -42,21 +42,21 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     <Bar
       data={{
         labels: ['Infected', 'Recovered', 'Deaths'],
-        dataSets: [
+        datasets: [
           {
             label: 'People',
             backgroundColor: [
-              'rgba(0,0,255,0.5)',
-              'rgba(0,255,0,0.5)',
-              'rgba(255,0,0,0.5)',
+              'rgba(0, 0, 255, 0.5)',
+              'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)',
             ],
             data: [confirmed.value, recovered.value, deaths.value],
           },
         ],
       }}
-      option={{
+      options={{
         legend: { display: false },
-        title: { display: true, tex: `Current State in ${country}` },
+        title: { display: true, text: `Current state in ${country}` },
       }}
     />
   ) : null;
